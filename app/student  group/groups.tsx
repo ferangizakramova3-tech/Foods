b25a199b518954130697616b63307a1e7d5b5853
Ferangiza <ferangizakramova3@gmail.com>
2026-06-18 22:19:36 +0500

    Guruh va o'quvchi turlarini qo'shish

--- app/student  group/groups.tsx
+++ app/student  group/groups.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from "react"
+import { supabase } from "@/lib/supabase"
+
+type Group = {
+  id: number
+  name: string
+  active: boolean
+}
+
+type Student = {
+  id: number
+  fullname: string
+  age: number
+  email: string
+  active: boolean
+  group_id: number
+}
 
 function groups() {
+
+const [groups, setGroups] = useState<Group[]>([])
+const [students, setStudents] = useState<Student[]>([])
+const [selectedGroupId, setSelectedGroupId] = useState<number | null>(null)
+const [search, setSearch] = useState("")
   return (
     <div>groups</div>
   )
